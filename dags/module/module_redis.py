c6874e5ddb0c84ff16c3e10c0c8c8b0a9ae6938a
= <=>
2022-01-06 09:07:04 +0000

    redis update

--- dags/module/module_redis.py
+++ dags/module/module_redis.py
@@ -1,8 +1,24 @@
 import redis
+from urllib.parse import urlparse
+import socket
+import json
 
-# 레디스 연결
-rd = redis.StrictRedis(host='13.209.89.224', port=53635, db=0)
+def connect_():
+    #
+    # 레디스 연결
+    try:
+        rd = redis.StrictRedis(host='15.165.124.23', port=58542, db=0)
+        print('연결 성공')
+    except:
+        print('연결 실패....')
+    return rd
 
-#rd.set("[키]", "[값]")
+def redis_set(rd,key,jsonData):
+    jsonDataDict = json.dumps(jsonData, ensure_ascii=False).encode('utf-8')
+    rd.set(key,jsonDataDict)
+    return "인 메모리 저장 완료"
+    
 
-print(rd.get("[키]").decode('utf-8'))
+def redis_get(rd,key):
+    value = rd.get(key)
+    return value
